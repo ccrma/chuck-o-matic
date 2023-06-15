e00eebd1e88e07db78ffb1ceaa7724dd4ee9818a
Ge Wang <ge@ccrma.stanford.edu>
2023-06-14 22:49:21 -0700

    update autoversion to prompt if no version number provided and print only changed lines

--- scripts/autoversion.sh
+++ scripts/autoversion.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+#
+# name: autoversion.sh
+# desc: update version number in chuck, miniAudicle, chugins
+#
+# usage: sh ./scripts/autoversion.sh <new-version-string>
 
-ReleaseName="chai"
+
+# version number
+VERSION_NUMBER=${1}
+
+# version name
+VERSION_NAME="chai"
+
+# prompt for version number if none provided
+while [ -z "${VERSION_NUMBER}" ]
+do
+    read -p "version number: " VERSION_NUMBER
+done
 
 # make sure the submodules are init and updated
+echo "updating submodules..."
 git submodule init
 git submodule update --recursive
 
+echo "updating version string in ./src/chuck/src/core/chuck.h..."
+sed -n "s/#define CHUCK_VERSION_STRING.*/#define CHUCK_VERSION_STRING                \"$VERSION_NUMBER ($VERSION_NAME)\"/gp" ./src/chuck/src/core/chuck.h
 
-# sh ./scripts/autoversion.sh <new-version-string>
+echo "updating version string in ./src/miniAudicle/src/verison.h..."
+sed -n "s/#define ENV_MA_VERSION.*/#define ENV_MA_VERSION \"${VERSION_NUMBER}\"/gp" ./src/miniAudicle/src/version.h
 
-echo "updating version string in ./src/chuck/src/core/chuck.h"
-
-sed "s/#define CHUCK_VERSION_STRING.*/#define CHUCK_VERSION_STRING                \"${1} ($ReleaseName)\"/g" ./src/chuck/src/core/chuck.h
-
-echo "updating version string in ./src/miniAudicle/src/verison.h"
-
-sed "s/#define ENV_MA_VERSION.*/#define ENV_MA_VERSION \"${1}\"/g" ./src/miniAudicle/src/version.h
-
-echo "updating version string in ./src/miniAudicle/src/verison.mk"
-sed "s/VERSION.*/VERSION?=${1}/g" ./src/miniAudicle/src/version.mk
-
-
-
+echo "updating version string in ./src/miniAudicle/src/verison.mk..."
+sed -n "s/VERSION.*/VERSION?=${VERSION_NUMBER}/gp" ./src/miniAudicle/src/version.mk

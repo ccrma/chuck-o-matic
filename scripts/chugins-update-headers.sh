69ee2e1a5b8f90300b46193f5ab6a256c0b57fa4
Ge Wang <ge@ccrma.stanford.edu>
2023-10-12 01:43:58 -0700

    update chugins cp

--- scripts/chugins-update-headers.sh
+++ scripts/chugins-update-headers.sh
@@ -10,8 +10,15 @@
 CHUCK_DIR=../chuck
 # base chugins directory
 CHUGINS_DIR="../chugins"
+# chuck headers repo
+CHEADERS_DIR="../cheaders"
 
 # print
 echo "copying headers to chugins/chuck/include"
 # copy to chugins/chuck/include
 cp -fv ${CHUCK_DIR}/src/core/*.h ${CHUGINS_DIR}/chuck/include/
+
+# print
+echo "copying headers to cheaders/include/"
+# copy to cheaders/include
+cp -fv ${CHUCK_DIR}/src/core/*.h ${CHEADERS_DIR}/include/
